c51acd3e1f71c1002340cd19d2554c10162510f0
Trisha Gee <trisha.gee@gmail.com>
2022-10-12 19:05:32 +0200

    [DA-1981] Fleshing out tests

--- src/test/java/com/getting2knowij/vcs/LogTest.java
+++ src/test/java/com/getting2knowij/vcs/LogTest.java
@@ -1,4 +1,13 @@
 package com.getting2knowij.vcs;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 public class LogTest {
+  @Test
+  @Disabled
+  void shouldFailBecauseWeHaveNotWrittenThisYet() {
+    Assertions.fail();
+  }
 }
