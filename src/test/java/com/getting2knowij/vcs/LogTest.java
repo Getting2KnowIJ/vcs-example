8dbd0c067c54421e2e60c25be8ffe31141fa6159
Trisha Gee <trisha.gee@gmail.com>
2022-10-12 20:08:34 +0200

    Should not commit straight away

--- src/test/java/com/getting2knowij/vcs/LogTest.java
+++ src/test/java/com/getting2knowij/vcs/LogTest.java
@@ -1,11 +1,17 @@
 package com.getting2knowij.vcs;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class LogTest {
   @Test
+  @Disabled
   void shouldFailBecauseWeHaveNotWrittenThisYet() {
     Assertions.fail();
+  }
+
+  public void unusedMethod() {
+    //TODO: should fail to commit first time because of a new warning
   }
 }
