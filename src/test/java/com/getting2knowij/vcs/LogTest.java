9e5a0e31dbca83c35e452ec2cd8d0109d9f48117
Trisha Gee <trisha.gee@gmail.com>
2022-10-12 20:00:15 +0200

    Working on the log test

---
+++ src/test/java/com/getting2knowij/vcs/LogTest.java
@@ -0,0 +1,11 @@
+package com.getting2knowij.vcs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LogTest {
+  @Test
+  void shouldFailBecauseWeHaveNotWrittenThisYet() {
+    Assertions.fail();
+  }
+}
