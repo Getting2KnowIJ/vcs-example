78e5548a2a68f1d0801c3f234cc0de2ee0cf58f9
Trisha Gee <trisha.gee@gmail.com>
2022-10-13 13:02:06 +0200

    [DA-1981] Sketching out the logging functionality

---
+++ src/main/java/com/getting2knowij/vcs/Log.java
@@ -0,0 +1,11 @@
+package com.getting2knowij.vcs;
+
+public class Log {
+  public Log(Out out) {
+
+  }
+
+  public void info(String message) {
+
+  }
+}
---
+++ src/main/java/com/getting2knowij/vcs/Out.java
@@ -0,0 +1,5 @@
+package com.getting2knowij.vcs;
+
+public interface Out {
+  boolean hasOutput(String message);
+}
