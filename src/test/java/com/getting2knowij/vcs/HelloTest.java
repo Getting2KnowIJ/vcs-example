678d212246b435b16c645224e43de295b493df3a
Trisha <trisha.gee@gmail.com>
2024-12-05 14:16:02 +1100

    [DA-8743] Adding a new test

--- src/test/java/com/getting2knowij/vcs/HelloTest.java
+++ src/test/java/com/getting2knowij/vcs/HelloTest.java
@@ -1,5 +1,6 @@
 package com.getting2knowij.vcs;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,5 +10,15 @@ class HelloTest {
   void shouldReturnCorrectString() {
     Hello hello = new Hello();
     assertEquals("Hello Git!", hello.getMessage());
+  }
+
+  @Test
+  @DisplayName("Should give an error if no name is added")
+  void shouldGiveAnErrorIfNoNameIsAdded() {
+      // given
+      Hello hello = new Hello();
+
+      // expect
+      assertEquals("You must provide an author name", hello.getMessage());
   }
 }
